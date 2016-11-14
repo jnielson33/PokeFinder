f517e260c9704996f7e64d15d9e48681cdc2bb59
jnielson33 <jnielson33@gmail.com>
2016-11-14 15:21:13 -0700

    Set up UI, Firebase, and Geofire

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Jared Nielson on 11/14/16.
+//  Copyright © 2016 Jared Nielson. All rights reserved.
+//
+
+#import "GeoFire.h"
+
